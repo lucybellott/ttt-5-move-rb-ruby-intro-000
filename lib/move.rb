86c86a2938c072df0cefbf5883062afd5ea206d6
patient-controller-1876 <lucybellott@gmail.com>
2021-03-01 22:40:39 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ def input_to_index(user_input)
 user_input.to_i - 1
 end
 
-board = [" ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def move(board, position, char = "X")
   board[position] = char
 end
