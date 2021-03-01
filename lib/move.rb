93a9fb82d63c468e63feeb0e84c994b5d2a174ba
patient-controller-1876 <lucybellott@gmail.com>
2021-03-01 21:13:41 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 # code your input_to_index and move method here!
 
 def input_to_index(user_input)
-user_input = ("5".to_i) - 1
+user_input = "5"
+converted_input = "5".to_i
 
 end
